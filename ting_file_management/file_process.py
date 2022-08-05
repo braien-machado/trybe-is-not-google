c980efb486eea255be9c5773b8886a410a848f65
Braien Machado de Paula <braienmp@outlook.com>
2022-08-05 16:50:38 -0300

    feat: file_process 'process' function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+
+def process(path_file, instance: Queue):
+    lines = txt_importer(path_file)
+
+    if not instance.in_queue(lines):
+        instance.enqueue(lines)
+
+        print({
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        })
 
 
 def remove(instance):
