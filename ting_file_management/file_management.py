bfd6c2db1bc67c31ed2c053deed222ce02f4cc11
Braien Machado de Paula <braienmp@outlook.com>
2022-08-05 16:20:43 -0300

    feat: txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+def txt_importer(path_file: str):
+    try:
+        if path_file.split('.')[-1] != 'txt':
+            raise ValueError('Formato inválido')
+
+        with open(path_file, "r") as file:
+            return file.read().split('\n')
+
+    except FileNotFoundError:
+        print(f'Arquivo {path_file} não encontrado', file=sys.stderr)
+    except ValueError:
+        print('Formato inválido', file=sys.stderr)
