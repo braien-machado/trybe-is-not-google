0be802fe56dbdefae00c67f6b8cd0377d2c55fab
Braien Machado de Paula <braienmp@outlook.com>
2022-08-05 18:07:46 -0300

    feat: word_search 'exists_word' function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,27 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+import re
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def exists_word(word: str, instance: Queue):
+    search_result = []
+    for index in range(len(instance)):
+        file_info = instance.search(index)
+        file_search_result = {
+            'palavra': word,
+            'arquivo': file_info['nome_do_arquivo'],
+            'ocorrencias': [],
+        }
+        for line, string in enumerate(file_info['linhas_do_arquivo']):
+            if word.casefold() in string.casefold():
+                file_search_result['ocorrencias'].append({
+                    'linha': line + 1,
+                })
+        
+        if len(file_search_result['ocorrencias']) > 0:
+            search_result.append(file_search_result)
+
+    return search_result
+
+
+def search_by_word(word: str, instance: Queue):
+    """search"""
